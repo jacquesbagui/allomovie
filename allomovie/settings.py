143a295538e02c49b3bf16730020695b2ecd9f98
Jean Jacques Bagui <jean-jacques@kaffein.agency>
2024-06-27 10:57:49 +0200

    Add core app with TimeStampedModel abstract base class

--- allomovie/settings.py
+++ allomovie/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gn*(18x42oi2&ri1uum7aju!2+sr99!pt&=0v(b&k3a@p@ocj_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'core',
     'movies'
 ]
 
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'allomovie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT')
     }
 }
 
--- movies/models.py
+++ movies/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from allomovie.core.models import TimeStampedModel
 
 
-class Actor(models.Model):
+class Actor(TimeStampedModel):
+    """
+    Actor Model
+    """
     first_name = models.CharField(max_length=100, help_text="Enter the first name")
     last_name = models.CharField(max_length=100, help_text="Enter the last name")
 
@@ -9,7 +13,10 @@ class Actor(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 
-class Movie(models.Model):
+class Movie(TimeStampedModel):
+    """
+    Actor Movie
+    """
     title = models.CharField(max_length=255, help_text="Enter the movie title")
     description = models.TextField(help_text="Enter the movie description")
     actors = models.ManyToManyField(Actor, help_text="Select actors for this movie")
